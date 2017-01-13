86fea193ce03f9eae7b303689921a28629685201
Simon Klit <boutrupsimon@gmail.com>
2017-01-13 20:13:20 +0100

    commit

--- bridge-script.py
+++ bridge-script.py
@@ -3,5 +3,5 @@
 import subprocess
 import sys
 
-subprocess.call("fixedngrokga &", shell=True)
+subprocess.call("fixedngrok &", shell=True)
 subprocess.call("ngrok " + " ".join(sys.argv[1:]), shell=True)
